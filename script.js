83acb02a009bc24204e4c97ca72488490124b62c
Chad Sundberg <chadsundberg@gmail.com>
2017-06-28 15:53:44 -0500

    postprime object challenge

---
+++ script.js
@@ -0,0 +1,32 @@
+var locations = []; // this array will hold your objects
+
+// object constructor
+function State(state, revenue, locationCount) {
+  this.state = state;
+  this.revenue = revenue;
+  this.locations = locationCount;
+  this.averageRevenue(){
+    return revenue / locations;
+  };
+}
+// object instances
+
+var illinois = new State("IL", 5000, 12);
+var minnesota = new State("MN", 300, 3);
+var nevada =  new State("NV", 25000, 1);
+
+// push object instances to locations array
+
+
+
+// DO NOT MODIFY THIS CODE
+$(document).ready(function () {
+  locations.forEach(function (element, index) {
+    var $li = $('<li></li>');
+    $li.append('<span>' +  element.state + '</span>');
+    $li.append('<span>' +  element.revenue + '</span>');
+    $li.append('<span>' +  element.locationCount + '</span>');
+    $li.append('<span>' +  element.averageRevenue() + '</span>');
+    $('ol').append($li);
+  });
+});
